1c671d87d288b95919db72e0be55b58b5cf6e317
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-06 08:57:35 -0800

    Day 1 - Hello Gophers!

--- Day1/helloworld.go
+++ Day1/helloworld.go
@@ -5,6 +5,7 @@ import "fmt" // Import "fmt" for input/output stuff
 func main() { // Main function, no surprise
 	fmt.Println("Hello World!") // Ah, hello old friend.
 	fmt.Println("Hello Beaux!")
+	fmt.Println("Hello Gophers!")
 }
 
 // Runs with: beauxwalton$ go run ./helloworld.go
