3dd88f8de150b58dfa3421455a3dfa9aa249028b
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-06 08:57:07 -0800

    Day 1 - Hello Gophers!

---
+++ Day1/helloworld.go
@@ -0,0 +1,10 @@
+package main // Package name declaration
+
+import "fmt" // Import "fmt" for input/output stuff
+
+func main() { // Main function, no surprise
+	fmt.Println("Hello World!") // Ah, hello old friend.
+	fmt.Println("Hello Beaux!")
+}
+
+// Runs with: beauxwalton$ go run ./helloworld.go
